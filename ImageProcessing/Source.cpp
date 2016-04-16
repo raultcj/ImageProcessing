b24c1a02f3071c9fba63be2bbe2396f2bd9509a4
Raul Juarez <raultcj@outlook.com>
2016-04-15 22:40:58 -0700

    d

---
+++ ImageProcessing/Source.cpp
@@ -0,0 +1,27 @@
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], IMREAD_COLOR); //Reads the file
+
+	if (!image.data) {
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	namedWindow("Display Window", WINDOW_AUTOSIZE);
+	imshow("Display Window", image);
+
+	waitKey(0);
+	return 0;
+}
